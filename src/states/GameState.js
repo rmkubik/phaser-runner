2a0fe2503273ad86aa5127c7c6c635d4aabd8fc5
rmkubik <rmkubik@me.com>
2017-12-10 01:06:52 -0800

    Add random height ground tiles

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -6,12 +6,14 @@ class GameState extends Phaser.State {
 		this.player;
 
 		const groundLength = 14;
-		const defaultGroundHeight = 1;
+		const minGroundHeight = 1;
+		const maxGroundHeight = 4;
 		this.groundData = [];
 		for (let i = 0; i < groundLength; i++) {
-			this.groundData.push(defaultGroundHeight);
+			this.groundData.push(this.game.rnd.integerInRange(minGroundHeight, maxGroundHeight));
 		}
 		this.ground = this.game.add.group();
+		this.game.stage.backgroundColor = "#3498db";
 	}
 
 	preload() {
@@ -22,12 +24,23 @@ class GameState extends Phaser.State {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
 		// let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
 		// text.anchor.set(0.5);
-		this.player = this.game.add.sprite(center.x, center.y, 'spritesheet', 'bee.png');
-		console.log(800 / this.player.width / 2);
+		this.player = this.game.add.sprite(center.x, center.y, 'spritesheet', 'alienGreen_walk1.png');
 		this.player.scale.setTo(0.5);
 
-		this.groundData.forEach((groundHeight) => {
-			this.game.ground.add.sprite()
+		// const tile = this.game.add.sprite(60, 60, 'spritesheet', 'grassMid.png');
+		// tile.scale.setTo(0.5);
+		// this.ground.add(tile);
+
+		this.groundData.forEach((groundHeight, index) => {
+			for (let y = 0; y < groundHeight; y++) {
+				let spriteKey = 'grassMid.png';
+				if (y < groundHeight - 1) {
+					spriteKey = 'grassCenter.png'
+				}
+				const tile = this.game.add.sprite(index * (64), this.game.height - ((y + 1) * 64), 'spritesheet', spriteKey);
+				tile.scale.setTo(0.5);
+				this.ground.add(tile);
+			}
 		})
 	}
 
