330d1b9fbafd9c49209dd00672d83301a7d9d544
rmkubik <rmkubik@me.com>
2017-12-10 22:15:07 -0800

    Adds collision between player and ground

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -21,31 +21,31 @@ class GameState extends Phaser.State {
 	}
 
 	create() {
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		// let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		// text.anchor.set(0.5);
-		this.player = this.game.add.sprite(center.x, center.y, 'spritesheet', 'alienGreen_walk1.png');
+		
+		this.player = this.game.add.sprite(0, 0, 'spritesheet', 'alienGreen_walk1.png');
 		this.player.scale.setTo(0.5);
+		this.game.physics.arcade.enable(this.player);
+		this.player.body.gravity.y = 300;
 
-		// const tile = this.game.add.sprite(60, 60, 'spritesheet', 'grassMid.png');
-		// tile.scale.setTo(0.5);
-		// this.ground.add(tile);
-
+		this.ground.enableBody = true;
 		this.groundData.forEach((groundHeight, index) => {
 			for (let y = 0; y < groundHeight; y++) {
 				let spriteKey = 'grassMid.png';
 				if (y < groundHeight - 1) {
 					spriteKey = 'grassCenter.png'
 				}
-				const tile = this.game.add.sprite(index * (64), this.game.height - ((y + 1) * 64), 'spritesheet', spriteKey);
+				const tile = this.ground.create(index * (64), this.game.height - ((y + 1) * 64), 'spritesheet', spriteKey);
 				tile.scale.setTo(0.5);
-				this.ground.add(tile);
+				tile.body.immovable = true;
 			}
 		})
 	}
 
 	update() {
-
+		this.game.physics.arcade.collide(this.player, this.ground);		
 	}
 
 }
